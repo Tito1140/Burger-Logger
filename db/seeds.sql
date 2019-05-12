6a879b8fc3ab771ea81a39ddf57a98ccab0ed570
John Tower <juan.c.torres1140@gmail.com>
2019-05-12 18:16:28 -0400

    Model View Setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Carolina", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jolly", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sunshine", false);
